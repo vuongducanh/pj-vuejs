3bfc870dccf591c49c55b165336ad2cdc8e3bfb0
anh.vuong <anh.vuong@tbwa.com>
2019-03-12 17:53:26 +0700

    add autocomplete

--- src/components/header-search/header-search-component.vue
+++ src/components/header-search/header-search-component.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="header-search">
-    <span>aa</span>
+    <div class="header-search__content">
+      <vue-single-select
+        :options="['apple','banana','cherry','tomato']"
+        :required="true"
+        value="aaaa">
+        </vue-single-select>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import VueSingleSelect from "vue-single-select";
+
 import "./header-search-component.scss";
 
-@Component
+@Component({
+  components: {
+    'vue-single-select': VueSingleSelect
+  }
+})
 export default class HeaderSearchComponent extends Vue {
   created() {
     this.$root.$on('mess', function(data: any) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,44 @@
 <template>
   <div id="home">
-     <header-component></header-component>
-     <header-search-component></header-search-component>
+    <header-component></header-component>
+    <header-search-component></header-search-component>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HeaderComponent from '../components/header/header-component.vue';
-import HeaderSearchComponent from '../components/header-search/header-search-component.vue';
-import "./home.scss"
-import axios from 'axios';
+import { Component, Vue } from "vue-property-decorator";
+import HeaderComponent from "../components/header/header-component.vue";
+import HeaderSearchComponent from "../components/header-search/header-search-component.vue";
+import BootstrapVue from "bootstrap-vue";
+Vue.use(BootstrapVue);
+
+import "./custom.scss";
+import "./home.scss";
+import axios from "axios";
 
 @Component({
   components: {
     HeaderComponent,
     HeaderSearchComponent
-  },
+  }
 })
-
 export default class Home extends Vue {
-  posts: any =  '';
+  posts: any = "";
   errors: any = [];
 
-   created() {
+  created() {
     this.getdata();
   }
 
   getdata() {
-     axios.get('http://demo4528318.mockable.io/demojson')
-    .then(response => {
-      this.posts = response.data;
-    })
-    .catch((e: any) => {
-      this.errors.push(e);
-    });
+    axios
+      .get("http://demo4528318.mockable.io/demojson")
+      .then(response => {
+        this.posts = response.data;
+      })
+      .catch((e: any) => {
+        this.errors.push(e);
+      });
   }
 }
 </script>
