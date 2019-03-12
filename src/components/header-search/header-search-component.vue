dee565fce6b19505f94891ad9e29bc8d091e2fbb
anh.vuong <anh.vuong@tbwa.com>
2019-03-12 23:07:09 +0700

    update auto complete

--- src/components/header-search/header-search-component.vue
+++ src/components/header-search/header-search-component.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="header-search">
     <div class="header-search__content">
-      <vue-single-select
-        :options="['apple','banana','cherry','tomato']"
-        :required="true"
-        value="aaaa">
-        </vue-single-select>
+      <div class="header-search__content-city">
+        <vue-single-select
+          :options="optionCity"
+          :required="true"
+          value="city"
+          v-model="valueSelect"
+          placeholder="Nhập điểm du lịch hoặc tên khách sạn"
+        ></vue-single-select>
+      </div>
     </div>
   </div>
 </template>
@@ -18,14 +22,17 @@ import "./header-search-component.scss";
 
 @Component({
   components: {
-    'vue-single-select': VueSingleSelect
+    "vue-single-select": VueSingleSelect
   }
 })
 export default class HeaderSearchComponent extends Vue {
+  optionCity: Array<any> = ['Seoul','Gangnam','Dongdaemun','VietNam'];
+  valueSelect: any = [];
+
   created() {
-    this.$root.$on('mess', function(data: any) {
-      console.log("header-search = ",data);
-    })
+    this.$root.$on("mess", function(data: any) {
+      console.log("header-search = ", data);
+    });
   }
 }
 </script>
