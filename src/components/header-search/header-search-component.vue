e6dde9706b7f0d026b92396b2dc099a1b385b4c8
anh.vuong <anh.vuong@tbwa.com>
2019-03-12 23:55:45 +0700

    update header search for agoda

--- src/components/header-search/header-search-component.vue
+++ src/components/header-search/header-search-component.vue
@@ -10,6 +10,18 @@
           placeholder="Nhập điểm du lịch hoặc tên khách sạn"
         ></vue-single-select>
       </div>
+      <div class="header-search__content-startdate">
+        <v-datepicker :value="state.startDate"></v-datepicker>
+      </div>
+      <div class="header-search__content-endate">
+        <v-datepicker :value="state.endDate"></v-datepicker>
+      </div>
+      <div class="header-search__content-addroom">
+        <input type="text">
+      </div>
+      <div class="header-search__content-search">
+        <button>Tìm Kiếm</button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,17 +29,23 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import VueSingleSelect from "vue-single-select";
+import Datepicker from "vuejs-datepicker";
 
 import "./header-search-component.scss";
 
 @Component({
   components: {
-    "vue-single-select": VueSingleSelect
+    "vue-single-select": VueSingleSelect,
+    "v-datepicker": Datepicker
   }
 })
 export default class HeaderSearchComponent extends Vue {
-  optionCity: Array<any> = ['Seoul','Gangnam','Dongdaemun','VietNam'];
+  optionCity: Array<any> = ["Seoul", "Gangnam", "Dongdaemun", "VietNam"];
   valueSelect: any = [];
+  state: Object = {
+    startDate: new Date(2019, 3, 12),
+    endDate: new Date(2019, 4, 12)
+  };
 
   created() {
     this.$root.$on("mess", function(data: any) {
