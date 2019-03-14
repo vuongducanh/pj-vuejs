a9b4f0a015ad609dc0fe930c4ec61d3f592731e6
anh.vuong <anh.vuong@tbwa.com>
2019-03-15 00:33:07 +0700

    css tab filter

--- src/components/tab-filter/tab-filter.vue
+++ src/components/tab-filter/tab-filter.vue
@@ -4,29 +4,29 @@
       <ul>
         <li class="tab-filter__content-item" @click="activeTab='1'" :class="{active:activeTab==='1'}">
           <div class="custom-checkbox custom--price">
-            <input name="price" id="checkbox-tab-all" type="radio" value="Mọi lựa chọn" v-model="valueTab">
+            <input id="checkbox-tab-all" type="radio" value="Mọi lựa chọn" v-model="valueTab">
             <label for="checkbox-tab-all">Mọi lựa chọn</label>
           </div>
         </li>
 
          <li class="tab-filter__content-item" @click="activeTab='2'" :class="{active:activeTab==='2'}">
           <div class="custom-checkbox custom--price">
-            <input name="price" id="checkbox-tab-all" type="radio" value="Khách sạn" v-model="valueTab">
-            <label for="checkbox-tab-all">Khách sạn</label>
+            <input id="checkbox-tab-hotel" type="radio" value="Khách sạn" v-model="valueTab">
+            <label for="checkbox-tab-hotel">Khách sạn</label>
           </div>
         </li>
 
          <li class="tab-filter__content-item" @click="activeTab='3'" :class="{active:activeTab==='3'}">
           <div class="custom-checkbox custom--price">
-            <input name="price" id="checkbox-tab-all" type="radio" value="Dành cho gia đình" v-model="valueTab">
-            <label for="checkbox-tab-all">Dành cho gia đình</label>
+            <input id="checkbox-tab-family" type="radio" value="Dành cho gia đình" v-model="valueTab">
+            <label for="checkbox-tab-family">Dành cho gia đình</label>
           </div>
         </li>
 
         <li class="tab-filter__content-item" @click="activeTab='4'" :class="{active:activeTab==='4'}">
           <div class="custom-checkbox custom--price">
-            <input name="price" id="checkbox-tab-all" type="radio" value="Agoda Homes" v-model="valueTab">
-            <label for="checkbox-tab-all">Agoda Homes</label>
+            <input id="checkbox-tab-agoda-home" type="radio" value="Agoda Homes" v-model="valueTab">
+            <label for="checkbox-tab-agoda-home">Agoda Homes</label>
           </div>
         </li>
 
@@ -41,7 +41,7 @@ import "./tab-filter.scss";
 
 @Component
 export default class TabFilterComponent extends Vue {
-    activeTab: number = 1;
-    valueTab: string = '';
+    activeTab: string = '1';
+    valueTab: string = 'Mọi lựa chọn';
 }
 </script>
