c09ee2a33775429f5d1886629636f19e1c056c0e
anh.vuong <anh.vuong@tbwa.com>
2019-03-17 12:16:50 +0700

    update tab sort

--- src/components/tab-filter-sort/tab-filter-sort.vue
+++ src/components/tab-filter-sort/tab-filter-sort.vue
@@ -3,39 +3,41 @@
     <div class="tab-filter-sort__content">
       <ul class="tab-filter-sort__content-list">
         <li class="tab-filter-sort__content-item tab-filter-sort__content-header">
-          <div>
-             <span>SẮP XẾP THEO</span>
-          </div>
+            <span>sắp xếp theo</span>
         </li>
         <li class="tab-filter-sort__content-item" @click="activeTab='1'" :class="{active:activeTab==='1'}">
-          <div class="custom-checkbox custom--price">
+          <div class="custom-checkbox">
             <input id="checkbox-tab-recommended" type="radio" value="search-sort-recommended" v-model="valueTabSort">
-            <label for="checkbox-tab-recommended">GỢI Ý</label>
+            <label for="checkbox-tab-recommended">gợi ý</label>
           </div>
         </li>
 
          <li class="tab-filter-sort__content-item" @click="activeTab='2'" :class="{active:activeTab==='2'}">
-          <div class="custom-checkbox custom--price">
+          <div class="custom-checkbox">
             <input id="checkbox-tab-price" type="radio" value="search-sort-price" v-model="valueTabSort">
             <label for="checkbox-tab-price">Giá thấp nhất trước</label>
           </div>
         </li>
 
         <li class="tab-filter-sort__content-item" @click="activeTab='3'" :class="{active:activeTab==='3'}">
-          <div class="custom-checkbox custom--price">
+          <div class="custom-checkbox">
             <input id="checkbox-tab-agoda-rating" type="radio" value="search-sort-guest-rating" v-model="valueTabSort">
             <label for="checkbox-tab-agoda-rating">Các nhận xét hàng đầu</label>
           </div>
         </li>
 
         <li class="tab-filter-sort__content-item" @click="activeTab='4'" :class="{active:activeTab==='4'}">
-          <div class="custom-checkbox custom--price">
+          <div class="custom-checkbox">
             <input id="checkbox-tab-secret-deals" type="radio" value="search-sort-secret-deals" v-model="valueTabSort">
-            <label for="checkbox-tab-secret-deals">ƯU ĐÃI BÍ MẬT</label>
+            <label for="checkbox-tab-secret-deals">Ưu đãi bí mật</label>
           </div>
         </li>
 
       </ul>
+    </div>
+    <div class="tab-filter-sort__barcontainer">
+      <i class="ficon ficon-couple"></i>
+      <span>Những nơi ở này chúng tôi lựa chọn dành riêng cho bạn.</span>
     </div>
   </div>
 </template>
